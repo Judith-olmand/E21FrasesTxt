e9067b5eea401776f039099fd6320df010eb4325
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-02-10 17:49:30 +0100

    añadida la prueba con file reader

--- src/E21FrasesTxt.java
+++ src/E21FrasesTxt.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class E21FrasesTxt {
 	public static void main(String[] args){
+        //CON BUFFERED READER
         try (BufferedReader reader = new BufferedReader(new FileReader("frases.txt"))) {
             String linea;
             while ((linea = reader.readLine()) != null) {
@@ -12,6 +13,19 @@ public class E21FrasesTxt {
         } catch (IOException e) {
             System.out.println("Ocurrió un error al leer el archivo: " +
                     e.getMessage());
+        }
+
+        System.out.println();
+        System.out.println();
+
+        //CON FILE READER
+        try (FileReader reader = new FileReader("frases.txt")) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                System.out.print((char) c);
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
         }
 
     }
